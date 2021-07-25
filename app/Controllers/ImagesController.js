c26a2334bb3f576c53b35d063fbd50abb7c190f5
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-24 21:09:07 -0600

    Image constructor & Template

--- app/Controllers/ImagesController.js
+++ app/Controllers/ImagesController.js
@@ -2,6 +2,13 @@ import { ProxyState } from "../AppState.js"
 import { imagesService } from "../Services/ImagesService.js"
 
 
+function _drawImage() {
+    const image = ProxyState.image
+    let template = ''
+    image.forEach(i => template += i.Template)
+    document.getElementById('image').innerHTML = template
+}
+
 export default class ImagesController {
 
     constructor() {
---
+++ app/Models/Image.js
@@ -0,0 +1,26 @@
+export default class Image {
+
+    constructor({ url, imgUrl, largeImgUrl, tags, author, query }) {
+        this.url = url
+        this.imgUrl = imgUrl || false
+        this.largeImgUrl = largeImgUrl
+        this.tags = tags
+        this.author = author
+        this.query = query
+    }
+    get Template() {
+        return `
+    <div class=" row text center p-2 m-2">
+        <div class="col-3" id="image">
+        ${this.url}
+        ${this.imgUrl}
+        ${this.largeImgUrl}
+        ${this.tags}
+        ${this.author}
+        ${this.query}
+        </div>
+    </div> `
+    }
+}
+
+
