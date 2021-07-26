dffda8c34d4a206ed324bff0b85141e618a2895f
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-25 21:31:14 -0600

    fix

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -7,16 +7,14 @@ export default class Todo {
         this.complated = complated || false
     }
     get Template() {
-        return `
-        <div class="card">
-        <div class="card-body m=1">
-                <input type="checkbox" value=""${this.completed ? 'checked' : ""} id="${this.id}"  onchange="app.todosController.updateTodo('${this.id}')">
+        return `<div class="custom-control custom-checkbox">
+            <input type="checkbox" value=""${this.completed ? 'checked' : ""} id="${this.id}"  onchange="app.todosController.updateTodo('${this.id}')">
                 <label class="col-10 form-check-label align-space-between" for="${this.user}">
                 ${this.description}
                 <span><i class="mdi mdi-15px mdi-trash-can" onclick="app.todosController.deleteTodo('${this.id}')"></i></span>
-        </div >
-        </div > `
+         </div>`
     }
-    // onclick="app.todosController.updateTodo('${this.id}')"
+    // onclick="app.todosController.updateTodo('${this.id}')"    <div class="card-body m=1"> 
+
 }
 
